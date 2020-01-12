fca88637ee7b710865e561910ace8d0e5c292b71
wang-pu-jun <952810538@qq.com>
2020-01-12 19:09:06 +0800

    Initial commit

---
+++ src/test/java/com/edu/lingkeedu/LingkeeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.lingkeedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingkeeduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
